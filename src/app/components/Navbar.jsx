17e12df7b7efa723c7a646254ecf9454cacfd318
kreifeur <kreifeur669@gmail.com>
2024-04-04 18:59:57 +0200

    menu responsive animation

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,31 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState  ,useRef} from "react";
 import { LiaMountainSolid } from "react-icons/lia";
 import { MdMenu } from "react-icons/md";
 import { CgClose } from "react-icons/cg";
+import TweenMax from "gsap";
+import { useLayoutEffect } from "react";;
 
 const Navbar = () => {
   const [flex, setFlex] = useState("hidden");
+  useLayoutEffect(() => {
+    TweenMax.fromTo(
+      ".box",
+      {
+        opacity: 0,
+        x: -20,
+      },
+      {
+        opacity: 1,
+        duration: 0.7,
+        x: 0,
+        delay: 0,
+        ease: "expo.inOut",
+      }
+    );
+    })
   return (
-    <div className="flex items-center justify-between px-8 h-[8vh] py-10">
+    <div className=" good flex items-center justify-between px-8 h-[8vh] py-10">
       <div className="z-[99]">
         <LiaMountainSolid className="text-5xl" />
       </div>
@@ -16,7 +34,7 @@ const Navbar = () => {
         className="z-[99]"
         onClick={() => (flex == "flex" ? setFlex("hidden") : setFlex("flex"))}
       >
-        {flex == 'hidden' ? <MdMenu className="text-4xl sm:hidden " /> : <CgClose className="text-4xl sm:hidden " /> }
+        {flex == 'hidden' ? <MdMenu className="text-4xl sm:hidden" /> : <CgClose className="text-4xl sm:hidden " /> }
       
       </div>
 
@@ -29,7 +47,7 @@ const Navbar = () => {
       </ul>
 
       <ul
-        className={`${flex} flex-col items-center justify-center gap-8 sm:hidden absolute bg-black top-0 w-screen h-screen left-0`}
+        className={`${flex} box flex-col items-center justify-center gap-8 sm:hidden absolute bg-black top-0 w-screen h-screen left-0`}
       >
         <li>Start Here</li>
         <li>Our Blog</li>
