904bec80b075d038194831204c5f87bf5c3ceab4
kreifeur <kreifeur669@gmail.com>
2024-04-04 18:40:01 +0200

    menu responsive

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { LiaMountainSolid } from "react-icons/lia";
 import { MdMenu } from "react-icons/md";
+import { CgClose } from "react-icons/cg";
 
 const Navbar = () => {
+  const [flex, setFlex] = useState("hidden");
   return (
     <div className="flex items-center justify-between px-8 h-[8vh] py-10">
-      <div>
+      <div className="z-[99]">
         <LiaMountainSolid className="text-5xl" />
       </div>
 
-      <div>
-        <MdMenu className="text-4xl sm:hidden " />
+      <div
+        className="z-[99]"
+        onClick={() => (flex == "flex" ? setFlex("hidden") : setFlex("flex"))}
+      >
+        {flex == 'hidden' ? <MdMenu className="text-4xl sm:hidden " /> : <CgClose className="text-4xl sm:hidden " /> }
+      
       </div>
 
       <ul className="sm:flex items-center gap-4 hidden">
+        <li>Start Here</li>
+        <li>Our Blog</li>
+        <li>Podcast</li>
+        <li>NewSitter</li>
+        <li className="font-bold">Get in Touch</li>
+      </ul>
+
+      <ul
+        className={`${flex} flex-col items-center justify-center gap-8 sm:hidden absolute bg-black top-0 w-screen h-screen left-0`}
+      >
         <li>Start Here</li>
         <li>Our Blog</li>
         <li>Podcast</li>
